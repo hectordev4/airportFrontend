2b20019c0bd6e17cace17c7f232ee8e9019e96b0
Hectordev4 <hector.valverde.cana@gmail.com>
2025-04-01 12:23:38 +0200

    feat: implement routing and context for flight services, add initial page components

---
+++ src/middleware/Context.jsx
@@ -0,0 +1,50 @@
+import { createContext, useContext } from "react";
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+const FlyService  = {
+    getAirports: async () => {
+        try{
+            const response = await axios.get(`${BASE_URL}/airports`);
+            return response.data;
+        }catch(error){
+            console.error("Error retrieving airports:", error);
+            throw error;
+        }
+    },
+    getFlights: async () => {
+        try{
+            const response = await axios.get(`${BASE_URL}/flights`);
+            return response.data;
+        }catch(error){
+            console.error("Error retrieving flights:", error);
+            throw error;
+        }
+    },
+    getPlanes: async () => {
+        try{
+            const response = await axios.get(`${BASE_URL}/planes`);
+            return response.data;
+        }catch(error){
+            console.error("Error retrieving planes:", error);
+            throw error;
+        }
+    },
+}
+
+const FlyServiceContext = createContext(FlyService);
+
+export const useFlyService = () => {
+    return useContext(FlyServiceContext);
+};
+
+export const FlyServiceProvider = ({ children }) => {
+    return (
+        <FlyServiceContext.Provider value={FlyService}>
+            {children}
+        </FlyServiceContext.Provider>
+    );
+};
+
+export default FlyService;
